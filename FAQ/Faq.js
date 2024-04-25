31d175d95cc6a15815906b146b99f1fc022ae435
dmabubakkar <dmabubakkar06@gmail.com>
2024-04-25 14:26:17 +0600

    ok

--- FAQ/Faq.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll(".accordion-item").forEach((item) => {
-    item.querySelector(".accordion-item-header").addEventListener("click", () => {
-      item.classList.toggle("open");
-    });
-  });
-  
